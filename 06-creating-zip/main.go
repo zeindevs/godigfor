517b42946ab0427ed036082bb7ce159d0395d690
zeindevs <zeindevs@gmail.com>
2024-07-10 21:26:14 +0700

    add: creating-zip

---
+++ 06-creating-zip/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+	"log"
+	"os"
+)
+
+func printUsage() {
+	fmt.Println("Usage:", os.Args[0], "<filepath>")
+	fmt.Println("Example:", os.Args[0], "document.txt")
+}
+
+func checkArgs() string {
+	if len(os.Args) < 2 {
+		printUsage()
+		os.Exit(1)
+	}
+	return os.Args[1]
+}
+
+func main() {
+	filename := checkArgs()
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Md5: %x\n\n", md5.Sum(data))
+	fmt.Printf("Sha1: %x\n\n", sha1.Sum(data))
+	fmt.Printf("Sha256: %x\n\n", sha256.Sum256(data))
+	fmt.Printf("Sha512: %x\n\n", sha512.Sum512(data))
+}
